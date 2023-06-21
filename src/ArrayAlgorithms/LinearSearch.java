314305cf9289be74683e47a831f3bc4e9859d1a1
Your Name <you@example.com>
2023-06-21 21:09:25 +0530

    Array commit

---
+++ src/ArrayAlgorithms/LinearSearch.java
@@ -0,0 +1,19 @@
+package ArrayAlgorithms;
+
+public class LinearSearch {
+    public static Integer linearSearch(int[] arr, int item) {
+        for (int current : arr) {
+            if (current == item) {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(linearSearch(arr, 1));
+        System.out.println(linearSearch(arr, 8));
+    }
+}
