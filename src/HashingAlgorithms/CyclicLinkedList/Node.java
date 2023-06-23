a04f39dcfbdd3ab9e57dd7f980531ff7b680c807
Your Name <you@example.com>
2023-06-23 08:18:41 +0530

    Hashing Commit

---
+++ src/HashingAlgorithms/CyclicLinkedList/Node.java
@@ -0,0 +1,10 @@
+package HashingAlgorithms.CyclicLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
---
+++ src/HashingAlgorithms/Employee/Employee.java
@@ -0,0 +1,14 @@
+package HashingAlgorithms.Employee;
+
+public class Employee {
+    String name;
+    int id;
+    String department;
+
+    public Employee(String name, int id, String department) {
+
+        this.name = name;
+        this.id = id;
+        this.department = department;
+    }
+}
